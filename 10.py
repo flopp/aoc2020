6cfa59a2c018e58e9c94787af43414ef9eed5cb8
florian <fpigorsch@gmail.com>
2020-12-10 19:49:39 +0100

    10.2 py

--- 10.py
+++ 10.py
@@ -10,3 +10,14 @@ for rating in adapters:
     last = rating
 print(diff[1] * diff[3])
 
+print("PART2")
+
+def arrangements(joltage, adapters, arrangements_from):
+    if joltage in arrangements_from:
+        return arrangements_from[joltage]
+    if joltage not in adapters:
+        return 0
+    arrangements_from[joltage] = sum(arrangements(joltage + d, adapters, arrangements_from) for d in range(1, 4))
+    return arrangements_from[joltage]
+
+print(arrangements(0, set(adapters).union({0}), {max(adapters) + 3: 1}))
