2ef2736398a0556d6ae4d0d0eaa4abe3c4594b9c
florian <fpigorsch@gmail.com>
2020-12-11 18:47:29 +0100

    11.2 py

--- 11.py
+++ 11.py
@@ -1,41 +1,66 @@
 import sys
 
 grid = [[char for char in line.strip()] for line in sys.stdin]
+directions = [(-1, -1), (-1, 0), (-1, +1), (0, -1), (0, +1), (+1, -1), (+1, 0), (+1, +1)]
 
-def sim(g):
-    g2 = []
+
+def look_part1(g, x, y, w, h):
+    global directions
+    around = 0
+    for dy, dx in directions:
+        y2 = y + dy
+        x2 = x + dx
+        if (0 <= y2 < h) and (0 <= x2 < w):
+            around += (g[y2][x2] == '#')
+    return around
+
+
+def look_part2(g, x, y, w, h):
+    global directions
+    around = 0
+    for dy, dx in directions:
+        y2 = y
+        x2 = x
+        while True:
+            y2 += dy
+            x2 += dx
+            if (0 <= y2 < h) and (0 <= x2 < w):
+                c = g[y2][x2]
+                if c == '#':
+                    around += 1
+                    break
+                if c == 'L':
+                    break
+            else:
+                break
+    return around
+
+
+def sim(g, look, sensitivity):
+    changed = True
     w = len(g[0])
     h = len(g)
-    for y in range(h):
-        r2 = []
-        for x in range(w):
-            current = g[y][x]
-            if current == '.':
-                r2.append('.')
-                continue
-            around = 0
-            for y2 in range (max(0, y-1), min(h, y+2)):
-                for x2 in range (max(0, x-1), min(w, x+2)):
-                    #print(f'y={y2}/{h} x={x2}/{w}')
-                    if y2 == y and x2 == x:
-                        continue
-                    around += (g[y2][x2] == '#')
-            if current == 'L' and around == 0:
-                r2.append('#')
-            elif current == '#' and around >= 4:
-                r2.append('L')
-            else:
-                r2.append(current)
-        g2.append(r2)
-    return g2
+    while changed:
+        changed = False
+        g2 = []
+        for y, r in enumerate(g):
+            r2 = list(r)
+            for x, c in enumerate(r):
+                if c == 'L':
+                    if look(g, x, y, w, h) == 0:
+                        r2[x] = '#'
+                        changed = True
+                elif c == '#':
+                    if look(g, x, y, w, h) >= sensitivity:
+                        r2[x] = 'L'
+                        changed = True
+            g2.append(r2)
+        g = g2
+    return sum(r.count('#') for r in g)
 
 
 print("PART1")
-while True:
-    new_grid = sim(grid)
-    if new_grid == grid:
-        break
-    grid = new_grid
-print(sum(r.count('#') for r in grid))
+print(sim(grid, look_part1, 4))
 
-
+print("PART2")
+print(sim(grid, look_part2, 5))
